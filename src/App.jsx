ed54ef2a1b548379bb0a8249a1e7b6773f0c65d0
Abdul Ahad <99830052+AbdulAhadilyas@users.noreply.github.com>
2023-01-12 21:39:24 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -2,179 +2,245 @@ import Nav from "./component/nav/Nav";
 import "./App.css";
 import Input from "./component/inputs and button/Input";
 import Post from "./component/post/Post";
-import axios from 'axios';
+import axios from "axios";
 import { useState, useEffect } from "react";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 import Danger from "./component/Alert/Danger";
-import Modal from "./component/modal/Modal"
+import Modal from "./component/modal/Modal";
+import FileUpload from "./component/modal/fileUplodModal";
+
 export const App = () => {
-
-  const socket = io.connect("http://localhost:8000", { transports: ['websocket'] });
-
-
+  const socket = io.connect("http://localhost:8000", {
+    transports: ["websocket"],
+  });
+  const baseUrl = process.env.BaseUrl || "http://localhost:8000/api/v1"
   const [toDoData, setToDoData] = useState([]);
   const [data, setData] = useState([]);
-  const [soketTodo, setSoketTodo] = useState({});
+  const [soketTodo, setSoketTodo] = useState(null);
   const [classID, setClassID] = useState("");
   const [isCLick, setIsCLick] = useState(false);
   const [errorState, setErrorState] = useState(false);
   const [isError, setsError] = useState(false);
   const [fetchData, setFetchData] = useState(false);
   const [open, setOpen] = useState(false);
-  const [publicIp, setPublicIp] = useState(false);
+  const [upLoadModal, setUpLoadModal] = useState(false);
+  const [publicIp, setPublicIp] = useState("");
   const [adminPassword, setAdminPassword] = useState("");
   const [ifPassNotMatch, setIfPassNotMatch] = useState(false);
-  const admin = "Saylani9321"
+  const [uploadFile, seTuploadFile] = useState("");
+  const [url, setUrl] = useState("");
+  const [fileLoader, setFileLoader] = useState(false);
+  const [progressPer, setprogressPer] = useState("")
+
+  const admin = "Saylani9321";
 
   useEffect(() => {
-    socket.on('connection', () => {
-      console.log("connected")
+    socket.on("connection", () => {
+      console.log("connected");
     });
-  }, [])
-
-
+    // eslint-disable-next-line
+  }, []);
 
   const getClass = async (val) => {
-    setClassID(val.classId)
-    await axios.get(`http://localhost:8000/data/getItem/${val.classId}`)
+    setClassID(val.classId);
+    await axios
+      .get(`${baseUrl}/getItem/${val.classId}`)
       .then(function (response) {
-        // console.log(response.data)
-        setData(response.data)
+        setData(response.data);
+        console.log("response.data:", response.data);
         if (response.data.error) {
-          setToDoData([])
-          setsError(response.data.error)
-          setErrorState(true)
+          setToDoData([]);
+          setsError(response.data.error);
+          setErrorState(true);
           setTimeout(() => {
-            setErrorState(false)
+            setErrorState(false);
           }, 1500);
         } else {
-          setToDoData(response.data[0].classData)
+          setToDoData(response.data[0].classData);
         }
       })
       .catch(function (error) {
         console.log(error);
       })
-      .finally(function () {
-
-      });
-    await axios.get(`http://localhost:8000/${`ip`}`)
+      .finally(function () { });
+    await axios
+      .get(`http://localhost:8000/${`ip`}`)
       .then(function (response) {
-        console.log(response.data.split(":"))
+        setPublicIp(userIp(response.data));
       })
-      .catch(function (error) {
-      })
-      .finally(function () {
-      });
-  }
+      .catch(function (error) { })
+      .finally(function () { });
+  };
   useEffect(() => {
     const getAllData = async () => {
-      await axios.get(`http://localhost:8000/data/getItem/${classID}`)
-        .then(function (response) {
-          // console.log(response.data)
-          setData(response.data)
-          if (response.data.error) {
-            setToDoData([])
-            setsError(response.data.error)
-            setErrorState(true)
-            setTimeout(() => {
-              setErrorState(false)
-            }, 1500);
-          } else {
-            setToDoData(response.data[0].classData)
-            console.log(response.data[0].classData)
-          }
-        })
-        .catch(function (error) {
+      if (classID === "") {
+        await axios
+          .get(`${baseUrl}/getItem/${classID}`)
+          .then(function (response) {
+            console.log(response.data);
+            setData(response.data);
+            if (response.data.error) {
+              setToDoData([]);
+              setsError(response.data.error);
+              setErrorState(true);
+              setTimeout(() => {
+                setErrorState(false);
+              }, 1500);
+            } else {
+              setToDoData(response.data[0].classData);
+              console.log(response.data[0].classData);
+            }
+          })
+          .catch(function (error) { })
+          .finally(function () { });
+      } else {
+        console.log("no class sec");
+      }
+    };
+    getAllData();
+    // eslint-disable-next-line
+  }, []);
 
-        })
-        .finally(function () {
-
-        });
-    }
-    getAllData()
-  }, [])
-
-  useEffect(() => {
-    socket.on('chat message', (msg) => {
-       setSoketTodo(msg)
-       });
-   }, [fetchData])
- 
-
-
+  const userIp = (val) => {
+    return val.split(":").slice(-1)[0];
+  };
   const getInput = async (val) => {
     if (classID === "") {
-      setErrorState(true)
+      setErrorState(true);
       setTimeout(() => {
-        setErrorState(false)
+        setErrorState(false);
       }, 1500);
     } else {
-      socket.emit('chat message', {
-        "text": val.inputText,
-        "cType": classID.toString()
+      await socket.emit("chat message", {
+        text: val.inputText,
+        cType: classID.toString(),
+        ip:publicIp
       });
-      await axios.post(`http://localhost:8000/data/addItem/${data[0]._id}`, {
-        "text": val.inputText,
-        "cType": classID.toString()
-      })
-        .then(function (response) {
-          setToDoData((prev)=> [...soketTodo,...toDoData])
+      await axios
+        .post(`${baseUrl}/addItem/${data[0]._id}`, {
+          text: val.inputText,
+          cType: classID.toString(),
+          ip:publicIp
         })
+        .then(function (response) { })
         .catch(function (error) {
           console.log(error);
         })
-        .finally(function () {
-        });
-     
+        .finally(function () { });
     }
-    setFetchData(!fetchData)
-    setIsCLick(!isCLick)
-  }
-  
- 
+    setFetchData(!fetchData);
+    setIsCLick(!isCLick);
+  };
 
-
-  const handleOpenModal = () => {
-    setOpen(true);
-  }
   const clearAll = async () => {
     if (adminPassword === admin) {
-      console.log("matched ")
-      setIfPassNotMatch(false)
-      await axios.delete(`http://localhost:8000/data/delete/${data[0]._id}/${classID}`)
+      console.log("matched ");
+      setIfPassNotMatch(false);
+
+      await axios
+        .delete(`${baseUrl}/delete/${data[0]._id}/${classID}`)
         .then(function (response) {
-          console.log(response.data)
-          setToDoData([])
+          console.log(response.data);
+          setToDoData([]);
         })
         .catch(function (error) {
           console.log(error);
         })
-        .finally(function () {
-        });
-
-
+        .finally(function () { });
     } else {
-      console.log("not matched ")
-      setIfPassNotMatch(true)
+      console.log("not matched ");
+      setIfPassNotMatch(true);
       setTimeout(() => {
-        setIfPassNotMatch(false)
+        setIfPassNotMatch(false);
       }, 1200);
     }
-
-  }
-
+  };
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
   const handleClose = () => {
     setOpen(false);
   };
 
-  const adminPass = (val) => {
-    setAdminPassword(val)
+  const postFile = async () => {
+    setFileLoader(true)
+    let formData = new FormData();
+    formData.append("todoFile", uploadFile);
+    // console.log(uploadFile.type)
+    const options = {
+      onUploadProgress: (progressEvent) => {
+        const { loaded, total } = progressEvent;
+        let percent = Math.floor((loaded * 100) / total)
+        console.log(`${loaded}kb of ${total}kb | ${percent}%`);
+        setprogressPer(percent)
+        console.log(progressPer)
+      }
+    }
+    try {
+      if (classID === "") {
+        console.log("no class selected")
+        setFileLoader(false)
+      } else {
+        const response = await axios.post(`${baseUrl}/upload`, formData, options)
+        setUrl(response.data);
+        socket.emit("chat message", {
+          url: response.data,
+          fileType: getFileType(uploadFile.type),
+          cType: classID.toString(),
+          ip:publicIp
+        });
+        const imageTodo = await axios.post(
+          `${baseUrl}/addItem/${data[0]._id}`,
+          {
+            url: response.data,
+            cType: classID.toString(),
+            fileType: getFileType(uploadFile.type),
+            ip:publicIp
+          }
+        );
+        console.log("imageTodo", imageTodo);
+        setFileLoader(false)
+      }
+    } catch (error) {
+      console.log("axios ", error);
+    }
+    console.log(getFileType(uploadFile.type));
   };
 
+  const uploadFilehandleClose = () => {
+    setUpLoadModal(false);
+  };
 
+  const uploadFilehandleOpen = () => {
+    setUpLoadModal(true);
+  };
+  const adminPass = (val) => {
+    setAdminPassword(val);
+  };
 
+  useEffect(() => {
+    const getSocketTodo = () => {
+      socket.on("chat message", (msg) => {
+        setSoketTodo(msg);
+        console.log(msg);
+      });
+    };
+    if (soketTodo) {
+      if (soketTodo.cType === classID) {
+        setToDoData([soketTodo, ...toDoData]);
+      }
+    }
+    getSocketTodo();
+    // eslint-disable-next-line
+  }, [soketTodo]);
 
-
+  const getFileType = (val) => {
+    let type = "";
+    type = val.split("/");
+    console.log(type[1]);
+    return type[1];
+  };
+  console.log(publicIp)
   return (
     <>
       <Modal
@@ -183,12 +249,20 @@ export const App = () => {
         handleClose={handleClose}
         handleSubmit={clearAll}
         ifError={ifPassNotMatch}
+      
+      />
+    
+      <FileUpload
+        modalState={upLoadModal}
+        handleClose={uploadFilehandleClose}
+        handleSubmit={postFile}
+        postFile={seTuploadFile}
+        isLoading={fileLoader}
+        progress={Number(progressPer)}
       />
       <div className="alert">
         <div className="alert-left">
-          {(errorState) ?
-            <Danger errorTxt={isError} />
-            : null}
+          {errorState ? <Danger errorTxt={isError} /> : null}
         </div>
       </div>
       <header>
@@ -196,18 +270,24 @@ export const App = () => {
       </header>
       <main>
         <section className="top">
-          <Input getClass={getClass}
+          <Input
+            getClass={getClass}
             getInput={getInput}
             handleOpenModal={handleOpenModal}
+            uploadFilehandleOpen={uploadFilehandleOpen}
           />
         </section>
         <section className="main-content">
           <ul className="list">
-            {toDoData.map((eachToDo, i) => <Post text={eachToDo.text}
-              time={eachToDo.createOn}
-              key={i}
-            />)
-            }
+            {toDoData.map((eachToDo, i) => (
+              <Post
+                text={eachToDo?.text}
+                time={eachToDo?.createOn}
+                key={i}
+                fileType={eachToDo?.fileType}
+                fileUrl={eachToDo?.url}
+              />
+            ))}
           </ul>
         </section>
       </main>
